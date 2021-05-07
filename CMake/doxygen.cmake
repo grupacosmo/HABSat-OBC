8ab6de57b7c33bb2a0c315135b6e91f5d25827bd
Wiktor Więcław <56267052+wiktorwieclaw@users.noreply.github.com>
2021-05-07 18:51:28 +0200

    updated looks of docs

--- CMake/doxygen.cmake
+++ CMake/doxygen.cmake
@@ -2,9 +2,15 @@ find_package(Doxygen)
 
 if (DOXYGEN_FOUND)
     set(DOXYGEN_USE_MDFILE_AS_MAINPAGE "${CMAKE_SOURCE_DIR}/README.md")
+    set(DOXYGEN_HTML_EXTRA_STYLESHEET
+            "${CMAKE_SOURCE_DIR}/Docs/doxygen-awesome-css/doxygen-awesome.css"
+            "${CMAKE_SOURCE_DIR}/Docs/doxygen-awesome-css/doxygen-awesome-sidenav.css")
+    set(DOXYGEN_GENERATE_TREEVIEW YES)
     set(DOXYGEN_EXTRACT_ALL YES)
 
     doxygen_add_docs(docs
-            ${CMAKE_SOURCE_DIR}/Lib ${CMAKE_SOURCE_DIR}/Inc  ${CMAKE_SOURCE_DIR}/Src
+            ${CMAKE_SOURCE_DIR}/Lib
+            ${CMAKE_SOURCE_DIR}/Inc
+            ${CMAKE_SOURCE_DIR}/Src
             )
 endif (DOXYGEN_FOUND)
